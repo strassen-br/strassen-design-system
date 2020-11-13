85ad6169b827cf145be2f386dd27d14df9259786
Thales Agapito <thsousa101@gmail.com>
2020-11-13 14:53:12 -0300

    Add config to emit declaration files

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,8 +1,9 @@
+/* eslint-disable max-len */
 import _Vue from 'vue';
 
-export {
-  PropsTypes as StButtonProps,
-  EventsTypes as StButtonEvents,
-} from '../src/components/atoms/StButton';
+export { PropsTypes as StButtonProps, EventsTypes as StButtonEvents } from './src/components/atoms/StButton/StButton.d';
+export { PropsTypes as StIconProps, EventsTypes as StIconEvents } from './src/components/atoms/StIcon/StIcon.d';
+export { PropsTypes as StLinkProps, EventsTypes as StLinkEvents } from './src/components/atoms/StLink/StLink.d';
+export { PropsTypes as StTextInputProps, EventsTypes as StTextInputEvents } from './src/components/atoms/StTextInput/StTextInput.d';
 
 export function install (Vue: typeof _Vue): void
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
@@ -11,5 +12,24 @@ module.exports = {
     config.resolve
       .plugin('tsconfig-paths')
       .use(tsconfigPathsPlugin);
+    // These are some necessary steps changing the default webpack config of the Vue CLI
+    // that need to be changed in order for Typescript based components to generate their
+    // declaration (.d.ts) files.
+    //
+    // Discussed here https://github.com/vuejs/vue-cli/issues/1081
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rule('ts').uses.delete('cache-loader');
+
+      config.module
+        .rule('ts')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap((opts) => {
+          opts.transpileOnly = false;
+          opts.happyPackMode = false;
+          return opts;
+        });
+    }
   },
+  parallel: false,
 };
