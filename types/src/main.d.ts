bfce1a0ec6e9e823eee7da5a007ac23aa683e585
Thales Agapito <thsousa101@gmail.com>
2020-11-14 15:21:55 -0300

    Add updated declaration types

--- types/src/main.d.ts
+++ types/src/main.d.ts
@@ -4,8 +4,11 @@ import './assets/index.css';
 import StButton from './components/atoms/StButton/StButton.vue';
 import StIcon from './components/atoms/StIcon/StIcon.vue';
 import StLink from './components/atoms/StLink/StLink.vue';
+import StTextInput from './components/atoms/StTextInput/StTextInput.vue';
 export declare function install(Vue: VueConstructor): void;
-export { StButton, StIcon, StLink };
+export {
+  StButton, StIcon, StLink, StTextInput,
+};
 declare const plugin: {
     install: typeof install;
 };
