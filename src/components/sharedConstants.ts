e878465db695dcc83177e04277ba0b17275d2c3e
Thales Agapito <thsousa101@gmail.com>
2020-12-24 17:42:53 -0300

    Add new brand color

--- src/components/sharedConstants.ts
+++ src/components/sharedConstants.ts
@@ -1,7 +1,8 @@
+/* eslint-disable max-len */
 export type ComponentSize = 'xs' | 'sm' | 'md' | 'lg'
 export const componentSizes: readonly ComponentSize[] = <const> ['xs', 'sm', 'md', 'lg'];
 
-export type ComponentColor = 'primary' | 'success' | 'danger';
-export const componentColors: readonly ComponentColor[] = <const> ['primary', 'success', 'danger'];
+export type ComponentColor = 'primary' | 'success' | 'danger' | 'brand';
+export const componentColors: readonly ComponentColor[] = <const> ['primary', 'success', 'danger', 'brand'];
 
 export type UnionSubset<T, U extends T> = U;
