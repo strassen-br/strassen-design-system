4643eb1853545539a25bc7d234064947cfea6fb2
Thales Agapito <thsousa101@gmail.com>
2021-03-02 14:40:56 -0300

    Add text input to main exports

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Plugin } from 'vue'
 import StButton from './components/atoms/StButton.vue'
 import StLink from './components/atoms/StLink.vue'
 import StIcon from './components/atoms/StIcon.vue'
+import StTextInput from './components/atoms/StTextInput.vue'
 import './index.css'
 
 const plugin: Plugin = {
@@ -9,6 +10,7 @@ const plugin: Plugin = {
     app.component('StButton', StButton)
     app.component('StLink', StLink)
     app.component('StIcon', StIcon)
+    app.component('StTextInput', StTextInput)
   },
 }
 
